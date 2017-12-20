b014ef31cb2a1df738e1175a44e8aa62579b987e
Fernando Moreira <nandomoreira.me@gmail.com>
2017-12-19 22:26:03 -0200

    travis deploy

---
+++ cibuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# skip if build is triggered by pull request
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# cleanup "_site"
+rm -rf _site
+mkdir _site
+
+yarn build
+bundle exec jekyll build
+bundle exec htmlproofer ./_site
